28aa8ae810d77e35b52ab53def5da7db93cb9f7a
Leonardo Figueiredo <leommko@gmail.com>
2020-08-07 04:37:41 -0300

    feat: add backend connection to frontend day 03

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,30 +2,52 @@ import React from 'react'
 
 import whatsAppIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number
+  avatar: string
+  bio: string
+  cost: number
+  name: string
+  subject: string
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (<article className="teacher-item">
     <header>
-      <img src="https://avatars3.githubusercontent.com/u/60974999?s=460&u=25fe7272944c3349f30d6bfce38ce4331c64a2f9&v=4" alt="Leonardo Figueiredo" />
+      <img src={teacher.avatar} alt={teacher.name} />
       <div>
-        <strong>Leonardo Figueiredo</strong>
-        <span>Matemática</span>
+        <strong>{teacher.name}</strong>
+        <span>{teacher.subject}</span>
       </div>
     </header>
     <p>
-      Texto descritivo do professor.
-  </p>
+      {teacher.bio}
+    </p>
 
     <footer>
       <p>
         Preço/hora
-      <strong>R$ 80,00</strong>
+      <strong>R$ {teacher.cost}</strong>
       </p>
-      <button type="button">
+      <a target="_blank" rel="noopener noreferrer" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
         <img src={whatsAppIcon} alt="WhatsApp" />
       Entrar em contato
-    </button>
+    </a>
     </footer>
   </article>)
 }
